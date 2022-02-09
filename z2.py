d3e74ee8a76cc1408c5263665c8b01fc1c731df0
EduardKolpakov <94556805+EduardKolpakov@users.noreply.github.com>
2022-02-09 20:56:02 +0300

    Create z2.py

---
+++ z2.py
@@ -0,0 +1,91 @@
+import pygame
+import requests
+import sys
+import os
+
+import math
+
+from distance import lonlat_distance
+from geo import reverse_geocode
+from bis import find_business
+
+
+def ll(x, y):
+    return "{0},{1}".format(x, y)
+
+
+args = sys.argv[1:]
+
+
+class MapParams(object):
+    # Параметры по умолчанию.
+    def __init__(self):
+        self.lat = float(args[0])  # Координаты центра карты на старте.
+        self.lon = float(args[1])
+        self.zoom = args[2]  # Масштаб карты на старте.
+        self.type = "map"  # Тип карты на старте.
+        self.use_postal_code = False
+
+    # Преобразование координат в параметр ll
+    def ll(self):
+        return ll(self.lon, self.lat)
+
+
+# Создание карты с соответствующими параметрами.
+def load_map(mp):
+    map_request = "http://static-maps.yandex.ru/1.x/?ll={ll}&z={z}&l={type}".format(ll=mp.ll(),
+                                                                                    z=mp.zoom,
+                                                                                    type=mp.type)
+
+    response = requests.get(map_request)
+    if not response:
+        print("Ошибка выполнения запроса:")
+        print(map_request)
+        print("Http статус:", response.status_code, "(", response.reason, ")")
+        sys.exit(1)
+
+    # Запишем полученное изображение в файл.
+    map_file = "map.png"
+    try:
+        with open(map_file, "wb") as file:
+            file.write(response.content)
+    except IOError as ex:
+        print("Ошибка записи временного файла:", ex)
+        sys.exit(2)
+
+    return map_file
+
+
+# Создание холста с текстом.
+def render_text(text):
+    font = pygame.font.Font(None, 30)
+    return font.render(text, 1, (100, 0, 100))
+
+
+def main():
+    # Инициализируем pygame
+    pygame.init()
+    screen = pygame.display.set_mode((600, 450))
+
+    # Заводим объект, в котором будем хранить все параметры отрисовки карты.
+    mp = MapParams()
+
+    while True:
+        event = pygame.event.wait()
+        if event.type == pygame.QUIT:  # Выход из программы
+            break
+
+        # Загружаем карту, используя текущие параметры.
+        map_file = load_map(mp)
+
+        # Рисуем картинку, загружаемую из только что созданного файла.
+        screen.blit(pygame.image.load(map_file), (0, 0))
+        pygame.display.flip()
+
+    pygame.quit()
+    # Удаляем за собой файл с изображением.
+    os.remove(map_file)
+
+
+if __name__ == "__main__":
+    main()
